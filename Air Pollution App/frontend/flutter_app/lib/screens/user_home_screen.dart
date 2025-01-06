59b66f5b2fd17d87bd53ad0e5a969e6f99b2132c
sametbilek <sametovic10@gmail.com>
2025-01-06 22:26:02 +0300

    Connectivity özelliği için çalışmalara başlandı.

--- Air Pollution App/frontend/flutter_app/lib/screens/user_home_screen.dart
+++ Air Pollution App/frontend/flutter_app/lib/screens/user_home_screen.dart
@@ -1,7 +1,12 @@
+import 'dart:convert';  // JSON parsing için gerekli
 import 'package:flutter/material.dart';
 import 'package:flutter_app/services/location_service.dart';
 import 'package:flutter_app/services/location_city_service.dart';
 import 'package:geolocator/geolocator.dart';
+import 'package:flutter_app/notification/notification_service.dart';  // Bildirim servisini import et
+import 'package:http/http.dart' as http;  // HTTP isteği için import et
+import 'package:flutter_app/connectivity/connectivity_handler.dart';
+import 'package:flutter_app/connectivity/offline_data_handler.dart';
 
 class UserHomeScreen extends StatefulWidget {
   const UserHomeScreen({super.key});
@@ -32,7 +37,11 @@ class _UserHomeScreenState extends State<UserHomeScreen> {
         _longitude = position.longitude;
       });
 
+      // Şehir bilgisini al
       _getCityFromLocation();
+
+      // API'ye POST isteği gönder
+      await _getAirPollutionData(_latitude!, _longitude!);
     } catch (e) {
       print('Konum alınamadı: $e');
     }
@@ -45,66 +54,91 @@ class _UserHomeScreenState extends State<UserHomeScreen> {
     });
   }
 
+  Future<void> _getAirPollutionData(double lat, double lon) async {
+    const String url = 'http://10.0.2.2:4000/api/airPollution/location';
+
+    final response = await http.post(
+      Uri.parse(url),
+      headers: {'Content-Type': 'application/json'},
+      body: json.encode({'lat': lat, 'lon': lon}),
+    );
+
+    if (response.statusCode == 200) {
+      final data = json.decode(response.body);
+      final co = data['list'][0]['components']['co'];
+
+      if (co > 50) {
+        await Future.delayed(const Duration(seconds: 5));
+        NotificationService.showNotification(
+          title: "Hava Kirliliği Uyarısı",
+          body: "CO seviyesi yüksek! Şu anki CO seviyesi: $co",
+        );
+      }
+    } else {
+      print('API isteği başarısız: ${response.statusCode}');
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.blue[50],
       body: SafeArea(
-        child: SingleChildScrollView(
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Padding(
-                padding: const EdgeInsets.all(16.0),
-                child: Text(
-                  "HOŞ GELDİNİZ",
-                  style: TextStyle(
-                    fontSize: 24,
-                    fontWeight: FontWeight.bold,
-                    color: Colors.blue[900],
+        child: ConnectivityHandler(
+          onlineWidget: SingleChildScrollView(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Padding(
+                  padding: const EdgeInsets.all(16.0),
+                  child: Text(
+                    "HOŞ GELDİNİZ",
+                    style: TextStyle(
+                      fontSize: 24,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.blue[900],
+                    ),
                   ),
                 ),
-              ),
-
-              const SizedBox(height: 16),
-              // Enlem ve boylam bilgileri
-              Padding(
-                padding: const EdgeInsets.all(16.0),
-                child: Text(
-                  _latitude != null && _longitude != null
-                      ? "Enlem: $_latitude, Boylam: $_longitude"
-                      : "Konum bilgisi alınıyor...",
-                  style: TextStyle(
-                    fontSize: 18,
-                    fontWeight: FontWeight.bold,
-                    color: Colors.blue[900],
+                const SizedBox(height: 16),
+                // Enlem ve boylam bilgilerini göstermek
+                Padding(
+                  padding: const EdgeInsets.all(16.0),
+                  child: Text(
+                    _latitude != null && _longitude != null
+                        ? "Enlem: $_latitude, Boylam: $_longitude"
+                        : "Konum bilgisi alınıyor...",
+                    style: TextStyle(
+                      fontSize: 18,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.blue[900],
+                    ),
                   ),
                 ),
-              ),
-              // Şehir bilgileri
-              Padding(
-                padding: const EdgeInsets.all(16.0),
-                child: Text(
-                  "Şehir: $_city",
-                  style: TextStyle(
-                    fontSize: 18,
-                    fontWeight: FontWeight.bold,
-                    color: Colors.blue[900],
+                // Şehir bilgisini göstermek
+                Padding(
+                  padding: const EdgeInsets.all(16.0),
+                  child: Text(
+                    "Şehir: $_city",
+                    style: TextStyle(
+                      fontSize: 18,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.blue[900],
+                    ),
                   ),
                 ),
-              ),
-            ],
+              ],
+            ),
           ),
+          offlineWidget: const OfflineDataHandler(),
         ),
       ),
       bottomNavigationBar: BottomNavigationBar(
         items: const [
           BottomNavigationBarItem(icon: Icon(Icons.home), label: "Home"),
-          BottomNavigationBarItem(
-              icon: Icon(Icons.settings), label: "Settings"),
+          BottomNavigationBarItem(icon: Icon(Icons.settings), label: "Settings"),
         ],
       ),
     );
-
   }
 }
